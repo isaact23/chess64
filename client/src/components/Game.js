2abd66f7bcdbca98df2da498412e2b9a23163c95
shufflesninja <isaact23@vt.edu>
2021-02-12 10:09:35 -0500

    Add code to Game component; add code credits to README.md

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -9,9 +9,14 @@ export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            game: new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"),
             settings: this.props.settings,
             myTurn: (this.props.settings.color === "white")
         }
+    }
+
+    onDrop = ({ sourceSquare, targetSquare }) => {
+
     }
 
     render() {
@@ -19,12 +24,14 @@ export default class Game extends React.Component {
             <div className="gameComponent">
                 <div className="chessboard">
                     <Chessboard
-                        position="start"
+                        onDrop={this.onDrop}
+                        position={this.state.game.fen}
                         orientation={this.state.settings.color}
                         boardStyle={{
                             borderRadius: "5px",
                             boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`
                         }}
+
                     />
                 </div>
                 <div className="gameControls">
