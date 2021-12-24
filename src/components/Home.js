f4ead691bee92b85f6f92ccbc8d87ca80783cefd
Isaac Thompson <shufflesninja@gmail.com>
2021-12-23 19:21:52 -0500

    Add youtube link

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,6 +24,7 @@ export default class Home extends React.Component {
                 <div className="mainPanel">
                     <h1>Chess 64</h1>
                     <a href="https://github.com/isaact23/chess64"><p>GitHub link</p></a>
+                    <a href="https://www.youtube.com/watch?v=3rId2W7ZQkc"><p>Youtube link</p></a>
                 </div>
                 <div className="newGamePanel">
                     <h1>New game</h1>
